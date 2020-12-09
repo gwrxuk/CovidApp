ce87471e546cf75c80cb4c9b46c1e097460ef31d
Jung-Hua Liu <junghua.liu@mobility-x.com>
2020-12-09 21:47:49 +0800

    Initial Commit

---
+++ Covid19/Covid19App.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19App.swift
+//  Covid19
+//
+//  Created by Jung-Hua Liu on 2020/12/9.
+//
+
+import SwiftUI
+
+@main
+struct Covid19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
